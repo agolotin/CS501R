82d13124cea6b951e9db5ac3a0c50efd29ae4f83
Artem Golotin <artem.golotin@gmail.com>
2016-10-15 12:08:27 -0600

    Slurm submission script

---
+++ generative_adversarial_net_lab7/submit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --time=0:10:00   # walltime
+#SBATCH --ntasks=6   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --gres=gpu:4 # number of GPUs
+#SBATCH --mem-per-cpu=10240M   # memory per CPU core
+#SBATCH -J "tf_gan"   # job name
+#SBATCH --qos=test # tell them it's a test job to get it running asap
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+python gan_lab7.py
